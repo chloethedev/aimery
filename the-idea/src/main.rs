da9283195819f18516a29e26475cb3380d12dcbe
chloethedev <chloethedev@gmail.com>
2022-03-02 12:31:17 -0500

    started project and works with simple input from user. next to add randomness

--- the-idea/src/main.rs
+++ the-idea/src/main.rs
@@ -1,3 +1,25 @@
+// use rand::Rng;
+// use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("\n...darkness...\n\n\n");
+
+    
+    println!("...warmth...\n\n");
+
+
+    println!("name please?\n");
+
+    let mut user_name = String::new();
+
+    io::stdin()
+        .read_line(&mut user_name)
+        .expect("everlasting darkness relentlessly consumes...");
+
+    println!("\nhello, {}, welcome to Everglass.", user_name.trim());
+
+            
 }
+
---
+++ the-idea/target/debug/deps/the_idea-7d30273dd2bc617b.d
@@ -0,0 +1,5 @@
+/home/chloethedev/poetry/aimery/the-idea/target/debug/deps/the_idea-7d30273dd2bc617b: src/main.rs
+
+/home/chloethedev/poetry/aimery/the-idea/target/debug/deps/the_idea-7d30273dd2bc617b.d: src/main.rs
+
+src/main.rs:
